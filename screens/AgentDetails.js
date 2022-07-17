f5737798e2f6153c811ba731ef087da7c5ecdd5b
erkut.turkyilmaz <erkut.turkyilmaz@dia.com.tr>
2022-07-17 21:40:58 +0300

    Android fix and styling

--- screens/AgentDetails.js
+++ screens/AgentDetails.js
@@ -2,6 +2,7 @@ import { View, Text, Image, Dimensions, StyleSheet, ScrollView, FlatList } from 
 import React, {useEffect, useState} from 'react'
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 import { Navigation } from 'react-native-navigation';
+import colors from '../src/colors';
 
 const SLIDER_WIDTH = Dimensions.get('window').width
 const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7)
@@ -20,7 +21,7 @@ function AgentDetails(props) {
         }
       }
     })
-    if (props.item.fullPortrait && props.item.fullPortraitV2) {
+    if (props.item.fullPortrait && props.item.fullPortraitV2){
       let dataArray = [];
       dataArray.push({'img': props.item.fullPortrait})
       dataArray.push({'img': props.item.fullPortraitV2});
@@ -56,11 +57,11 @@ function AgentDetails(props) {
   const renderSkillItem = ({item, index}) => {
     return (
       <View style={styles.skillItemContainer}>
-        <View style={{flex: 0.2, justifyContent: 'center', alignItems: 'center'}}>
-          <Image style={{width: 50, height: 50}} source={{uri: item.displayIcon}}/>
+        <View style={styles.skillIconContainer}>
+          <Image style={styles.skillIcon} source={{uri: item.displayIcon}}/>
         </View>
-        <View style={{flex: 0.8,flexDirection: 'column'}}>
-          <Text style={{paddingBottom: 10, fontWeight: 'bold', fontFamily: 'Optima'}}>{item.displayName}</Text>
+        <View style={styles.skillContainer}>
+          <Text style={styles.skillName}>{item.displayName}</Text>
           <Text>{item.description}</Text>
         </View>
       </View>
@@ -71,8 +72,8 @@ function AgentDetails(props) {
     return (
       <ScrollView>
         <View style={styles.descriptionContainer}>
-          <Text style={{fontWeight: 'bold', paddingBottom: 10, fontSize: 20, fontFamily: 'Optima'}}>{props.item.displayName}</Text>
-          <Text style={{fontSize: 15, fontFamily: 'Optima'}}>{props.item.description}</Text>
+          <Text style={styles.heroName}>{props.item.displayName}</Text>
+          <Text style={styles.description}>{props.item.description}</Text>
         </View>
       </ScrollView>
     )
@@ -120,7 +121,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1, 
     flexDirection: 'column', 
-    backgroundColor: '#FFFFFF', 
+    backgroundColor: colors.PAGE_BACKGROUND, 
     paddingBottom: 10
   },
   carouselContainer: {
@@ -133,7 +134,7 @@ const styles = StyleSheet.create({
       width: 0,
       height: 3,
     },
-    shadowOpacity: 0.29,
+    shadowOpacity: 1,
     shadowRadius: 4.65,
     elevation: 7,
     justifyContent: 'center',
@@ -166,14 +167,51 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20, 
     paddingTop: 10,
     justifyContent: 'center', 
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: colors.PAGE_BACKGROUND,
+    margin: 10,
+    borderRadius: 20
   },
   skillItemContainer: {
     flexDirection: 'row', 
-    backgroundColor: '#FD4556', 
+    backgroundColor: colors.PAGE_BACKGROUND, 
     padding: 10,
     borderRadius: 30,
     marginHorizontal: 10
+  },
+  skillIcon: {
+    width: 50, 
+    height: 50, 
+    margin: 10
+  },
+  skillIconContainer: {
+    flex: 0.2, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: colors.CONTAINER_BACKGROUND, 
+    margin: 10, 
+    borderRadius: 20, 
+    alignSelf: 'center'
+  },
+  skillContainer: {
+    flex: 0.8,
+    flexDirection: 'column'
+  },
+  skillName: {
+    paddingBottom: 10, 
+    fontWeight: 'bold', 
+    fontFamily: 'Optima'
+  },
+  heroName: {
+    fontWeight: 'bold', 
+    paddingBottom: 10, 
+    fontSize: 20, 
+    fontFamily: 'Optima'
+  },
+  description: {
+    fontSize: 15, 
+    fontFamily: 'Optima', 
+    marginBottom: 20
   }
 })
 
